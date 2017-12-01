807e5955bfc7608998494027aa6e264fdef28104
msjo91 <msjo91@gmail.com>
2017-12-01 13:02:30 +0900

    Add STATIC_URL and TEMPLATES_DIR

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -39,6 +39,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     STATIC_DIR,
 )
+STATIC_URL = '/static/'
 
 # Template
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
@@ -79,7 +80,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
